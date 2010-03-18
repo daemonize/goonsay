b2a6d369da83041fc32c1a7b5f40cd7a9cbf2340
dstufft <donald.stufft@gmail.com>
2010-03-18 12:14:46 +0000

    finished rewriting vote.js to be generic

--- goonsay/apps/goonsay/media/js/vote.js
+++ goonsay/apps/goonsay/media/js/vote.js
@@ -1,49 +1,46 @@
 $(document).ready(function(){
-    var fadeout_delay = 5000;
-    $('.vote-up').click(function(){
-        var link = $(this).parent().find('.score-value');
+    var fadeout_delay = 3000;
+    var success_msg = '<div class="vote-msg"><span class="success">Thanks for Voting!</span></div>';
+    var error_msg_start = '<div class="vote-msg"><span class="error">';
+    var error_msg_end = '</span></div>';
+
+    function voting_handler()
+    {
+        var voting = $(this).parent();
+
+        voting.find('a').unbind('click');
+        voting.find('a').bind('click', function(){ return false; });
+
+        setTimeout(function(){
+            voting.find('a').unbind('click');
+            voting.find('a').bind('click', voting_handler);
+        }, fadeout_delay + 500);
+
         $.ajax({
             'url': $(this).attr('href'),
             'type': 'POST',
             'dataType': 'json',
             'success': function(data){
                 if( data.success ){
-                    link.text(data.score.score);
-                    $('#vote-msg').text('Thanks for Voting!').fadeIn('fast');
+                    voting.find('.score-value').text(data.score.score);
+                    voting.after(success_msg);
+                    voting.parent().find('.vote-msg').fadeIn('slow');
                     setTimeout(function(){
-                        $('#vote-msg').fadeOut('slow');
+                        voting.parent().find('.vote-msg').fadeOut('slow');
+                        voting.parent().find('.vote-msg').remove();
                     }, fadeout_delay);
                 } else {
-                    $('#vote-msg').text(data.error_message).fadeIn('fast');
+                    voting.after(error_msg_start + data.error_message + error_msg_end);
+                    voting.parent().find('.vote-msg').fadeIn('fast');
                     setTimeout(function(){
-                        $('#vote-msg').fadeOut('slow')
+                        voting.parent().find('.vote-msg').fadeOut('slow');
+                        voting.parent().find('.vote-msg').remove();
                     }, fadeout_delay);
                 }
             }
             });
         return false;
-    });
-    $('.vote-down').click(function(){
-        var link = $(this).parent().find('.score-value');
-        $.ajax({
-            'url': $(this).attr('href'),
-            'type': 'POST',
-            'dataType': 'json',
-            'success': function(data){
-                if( data.success ){
-                    link.text(data.score.score);
-                    $('#vote-msg').text('Thanks for Voting!').fadeIn('fast');
-                    setTimeout(function(){
-                        $('#vote-msg').fadeOut('slow');
-                    }, fadeout_delay);
-                } else {
-                    $('#vote-msg').text(data.error_message).fadeIn('fast');
-                    setTimeout(function(){
-                        $('#vote-msg').fadeOut('slow');
-                    }, fadeout_delay);
-                }
-            }
-            });
-        return false;
-    });
+    }
+
+    $('.vote-link').click(voting_handler);
 });
